e7781ac1fae764595aa8952fb7d5aae6e1d1f2a5
Hugo Ribeiro <ribeirohugo@outlook.pt>
2021-05-18 21:00:56 +0100

    Created Server package.

---
+++ cmd/server/server.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ribeirohugo/go_image_getter/internal/config"
+	"github.com/ribeirohugo/go_image_getter/internal/getter"
+	"github.com/ribeirohugo/go_image_getter/internal/server"
+)
+
+const configFile = "config.toml"
+
+func main() {
+	cfg, err := config.Load(configFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	imageGetter := getter.New(cfg)
+
+	serverHttp := server.New(imageGetter, cfg.Host)
+
+	serverHttp.GinGonic()
+}
