fde56a69d1a53263acba51b738ce26e68fd2151d
Hugo Ribeiro <56323467+ribeirohugo@users.noreply.github.com>
2022-08-13 01:21:47 +0100

    Add patterns package (#14)
    * Added patterns package with different regex patterns as consts.

---
+++ patterns/consts.go
@@ -0,0 +1,13 @@
+// Package patterns holds regex patterns to fetch specific content from a page
+package patterns
+
+const (
+	// HTMLTitle - HTML title between title tags
+	HTMLTitle = "(?:\\<title\\>)(.*)(?:<\\/title\\>)"
+
+	// ImageContentFromHrefURL - Href value with HTTP or HTTPS prefix and with jpg, gif or png format
+	ImageContentFromHrefURL = "href=[\"'](http[s]?://[a-zA-Z0-9/._-]+[.](?:jpg|gif|png))[\"']"
+
+	// ImageContentFromHrefURLWithoutHTTP Href value without any HTTP prefix and with jpg, gif or png format
+	ImageContentFromHrefURLWithoutHTTP = "href=[\"']([a-zA-Z0-9/._-]+[.](?:jpg|gif|png))[\"']"
+)
