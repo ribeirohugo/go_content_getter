8f1bbd200311f76f4c44d5df04de6210527aed25
Hugo Ribeiro <ribeirohugo@outlook.pt>
2022-08-13 15:05:15 +0100

    test: improved getter package test coverage

--- getter/getter_test.go
+++ getter/getter_test.go
@@ -70,21 +70,90 @@ func TestGetImageName(t *testing.T) {
 }
 
 func TestGetter_Get(t *testing.T) {
-	server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-		_, err := rw.Write([]byte(pageRequest))
-		require.NoError(t, err)
-	}))
-	defer server.Close()
+	t.Run("with no error return", func(t *testing.T) {
+		t.Run("with content return", func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				_, err := rw.Write([]byte(pageRequest))
+				require.NoError(t, err)
+			}))
+			defer server.Close()
 
-	getter := Getter{
-		url:          server.URL,
-		contentRegex: patterns.ImageContentFromHrefURLWithoutHTTP,
-	}
+			getter := Getter{
+				url:          server.URL,
+				contentRegex: patterns.ImageContentFromHrefURLWithoutHTTP,
+			}
 
-	images, title, err := getter.Get()
-	assert.Len(t, images, 1)
-	assert.Equal(t, "Page Title", title)
-	assert.NoError(t, err)
+			images, title, err := getter.Get()
+			assert.Len(t, images, 1)
+			assert.Equal(t, "Page Title", title)
+			assert.NoError(t, err)
+		})
+
+		t.Run("without no content return", func(t *testing.T) {
+			t.Run("no content found in response body", func(t *testing.T) {
+				server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+					_, err := rw.Write([]byte(pageRequest))
+					require.NoError(t, err)
+				}))
+				defer server.Close()
+
+				getter := Getter{
+					url: server.URL,
+				}
+
+				images, title, err := getter.Get()
+				assert.Len(t, images, 0)
+				assert.Equal(t, "Page Title", title)
+				assert.NoError(t, err)
+			})
+
+			t.Run("page status code is not ok", func(t *testing.T) {
+				server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+					rw.WriteHeader(http.StatusBadRequest)
+				}))
+				defer server.Close()
+
+				getter := Getter{
+					url:          server.URL + "/not_found",
+					contentRegex: patterns.ImageContentFromHrefURLWithoutHTTP,
+				}
+
+				images, title, err := getter.Get()
+				assert.Len(t, images, 0)
+				assert.Equal(t, "", title)
+				assert.NoError(t, err)
+			})
+
+			t.Run("invalid url", func(t *testing.T) {
+				getter := Getter{
+					url: "",
+				}
+
+				images, title, err := getter.Get()
+				assert.Len(t, images, 0)
+				assert.Equal(t, "", title)
+				assert.NoError(t, err)
+			})
+		})
+	})
+
+	t.Run("with error return", func(t *testing.T) {
+		t.Run("reading body response", func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				rw.Header().Set("Content-Length", "1")
+			}))
+			defer server.Close()
+
+			getter := Getter{
+				url: server.URL,
+			}
+
+			images, title, err := getter.Get()
+			assert.Len(t, images, 0)
+			assert.Equal(t, "", title)
+			assert.Error(t, err)
+		})
+	})
 }
 
 func TestGetter_Download(t *testing.T) {
@@ -92,31 +161,46 @@ func TestGetter_Download(t *testing.T) {
 
 	var getter = Getter{}
 
-	t.Run("with folder defined", func(t *testing.T) {
-		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			_, err := rw.Write([]byte("pageBody"))
+	t.Run("with no error return", func(t *testing.T) {
+		t.Run("with folder defined", func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				_, err := rw.Write([]byte("pageBody"))
+				require.NoError(t, err)
+			}))
+			defer server.Close()
+
+			err := getter.Download(folderName, []string{server.URL})
+			assert.NoError(t, err)
+
+			err = os.RemoveAll(folderName)
 			require.NoError(t, err)
-		}))
-		defer server.Close()
+		})
 
-		err := getter.Download(folderName, []string{server.URL})
-		assert.NoError(t, err)
+		t.Run("with empty folder name", func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				_, err := rw.Write([]byte("pageBody"))
+				require.NoError(t, err)
+			}))
+			defer server.Close()
 
-		err = os.RemoveAll(folderName)
-		require.NoError(t, err)
+			err := getter.Download("", []string{server.URL})
+			assert.NoError(t, err)
+
+			err = os.RemoveAll(defaultFolderName)
+			require.NoError(t, err)
+		})
 	})
 
-	t.Run("with empty folder name", func(t *testing.T) {
-		server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
-			_, err := rw.Write([]byte("pageBody"))
-			require.NoError(t, err)
-		}))
-		defer server.Close()
+	t.Run("with error return", func(t *testing.T) {
+		t.Run("creating file", func(t *testing.T) {
+			server := httptest.NewServer(http.HandlerFunc(func(rw http.ResponseWriter, req *http.Request) {
+				_, err := rw.Write([]byte("pageBody"))
+				require.NoError(t, err)
+			}))
+			defer server.Close()
 
-		err := getter.Download("", []string{server.URL})
-		assert.NoError(t, err)
-
-		err = os.RemoveAll(defaultFolderName)
-		require.NoError(t, err)
+			err := getter.Download(string(os.PathSeparator), []string{server.URL})
+			assert.Error(t, err)
+		})
 	})
 }
