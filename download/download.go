5e4b18fa96b90bd5a68e7ca9c40f5fd3a1a707d9
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-04-03 00:09:41 +0100

    wip: add download

--- download/download.go
+++ download/download.go
@@ -5,10 +5,11 @@ import (
 	"fmt"
 	"io"
 	"net/http"
+	"regexp"
 )
 
 // ContentBytes - Makes an HTTP request to a URL and
-// gets the content in bytes format, according to a given content regex pattern.
+// gets the content in bytes format.
 func ContentBytes(contentURL string) ([]byte, error) {
 	response, err := http.Get(contentURL) //nolint:gosec // received value needs to be a variable
 	if err != nil {
@@ -27,3 +28,55 @@ func ContentBytes(contentURL string) ([]byte, error) {
 
 	return []byte{}, fmt.Errorf("error response status: %s", response.Status)
 }
+
+// MultipleContentBytes - Makes an HTTP request to a URL and downloads the page title and the URLs inside that page,
+// to be downloaded.
+// It returns the titlel, the slice of URLs and an error (or nil).
+func TitleAndURLs(contentURL, titlePatterns, urlsPatterns string) (string, []string, error) {
+	contentBytes, err := ContentBytes(contentURL)
+	if err != nil {
+		return "", []string{}, err
+	}
+
+	contentString := string(contentBytes)
+
+	titleRegex := regexp.MustCompile(contentString)
+	contentRegex := regexp.MustCompile(contentString)
+
+	titleMatch := titleRegex.FindStringSubmatch(contentString)
+	contentMatch := contentRegex.FindAllStringSubmatch(contentString, -1)
+
+	title := ""
+	if len(titleMatch) > 1 {
+		title = titleMatch[1]
+	}
+
+	var urls []string
+	for _, url := range contentMatch {
+		urls = append(urls, url[1])
+	}
+
+	return title, urls, nil
+}
+
+// MultipleContentBytes - Makes an HTTP request to a URL download all the content from that page (for a given regex pattern)
+// and returns a map with content bytes for a given content name.
+// func MultipleContentBytes(contentURL, contentPattern string) (map[string][]byte, error) {
+// 	contentBytes, err := ContentBytes(contentURL)
+// 	if err != nil {
+// 		return map[string][]byte{}, err
+// 	}
+
+// 	bodyString := string(contentBytes)
+
+// 	contentRegex := regexp.MustCompile(contentPattern)
+
+// 	contentMatch := contentRegex.FindAllStringSubmatch(bodyString, -1)
+// 	// titleMatch := titleRegex.FindStringSubmatch(bodyString)
+
+// 	var urls []string
+// 	for _, url := range contentMatch {
+// 		urls = append(urls, url[1])
+// 	}
+
+// }
