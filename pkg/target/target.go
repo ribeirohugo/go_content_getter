60e8d68cb0e5a2bc26476f45373168a9cb4f951d
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-04-04 17:17:15 +0100

    chore: update `target` package

---
+++ pkg/target/target.go
@@ -0,0 +1,38 @@
+package target
+
+import (
+	"net/url"
+	"path"
+	"regexp"
+	"strings"
+
+	"github.com/ribeirohugo/go_content_getter/pkg/model"
+)
+
+func GetAll(page model.Page, pattern string) ([]model.Target, error) {
+	var (
+		targets []model.Target
+	)
+
+	contentRegex := regexp.MustCompile(pattern)
+	matches := contentRegex.FindAllStringSubmatch(string(page.Content), -1)
+	// matches := contentRegex.FindAllSubmatch(page.Content, -1)
+
+	for _, match := range matches {
+		targets = append(targets, model.Target{
+			URL:      match[1],
+			Filename: extractFilename(match[1]),
+		})
+	}
+
+	return targets, nil
+}
+
+func extractFilename(rawURL string) string {
+	parsed, err := url.Parse(rawURL)
+	if err != nil {
+		parts := strings.Split(rawURL, "/")
+		return parts[len(parts)-1]
+	}
+	return path.Base(parsed.Path)
+}
