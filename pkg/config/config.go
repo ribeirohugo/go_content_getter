bd0a1415c780f699a7f7373472b1dccca509ec85
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-04-05 18:16:32 +0100

    feat: add `config` v2 package

---
+++ pkg/config/config.go
@@ -0,0 +1,33 @@
+// Package config holds configuration data struct and data loading
+package config
+
+import (
+	"os"
+
+	"github.com/BurntSushi/toml"
+
+	"github.com/ribeirohugo/go_content_getter/pkg/patterns"
+)
+
+const defaultHost = "localhost:8080"
+
+// Load - loads configurations from a given toml file path
+func Load(filePath string) (Config, error) {
+	bytes, err := os.ReadFile(filePath)
+	if err != nil {
+		return Config{}, err
+	}
+
+	config := Config{
+		Host:         defaultHost,
+		ContentRegex: patterns.ImageContentFromHrefURL,
+		TitleRegex:   patterns.HTMLTitle,
+	}
+
+	err = toml.Unmarshal(bytes, &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
