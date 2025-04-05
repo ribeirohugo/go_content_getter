30d15b8db67628b6cdd5247b79c5c5477c608e26
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-04-05 14:50:32 +0100

    chore: update `ImageSrc` regex pattern

--- pkg/patterns/consts.go
+++ pkg/patterns/consts.go
@@ -5,11 +5,8 @@ const (
 	// HTMLTitle - HTML title between title tags
 	HTMLTitle = "(?:\\<title\\>)(.*)(?:<\\/title\\>)"
 
-	// HTMLTitle2 - HTML title between title tags
-	HTMLTitle2 = "`(?i)<title[^>]*>(.*?)</title>`"
-
 	// ImageSrc - is HTML image full URL source value
-	ImageSrc = "src=[\"'](http[s]?://[a-zA-Z0-9/._-]+[.](?:jpg|gif|png))(?:[?&#].*)?[\"']"
+	ImageSrc = "src=[\"'](http[s]?://[a-zA-Z0-9/._-]+(?::[0-9]+)?/[a-zA-Z0-9/._-]*[.](?:jpg|gif|png))(?:[?&#].*)?[\"']"
 
 	// ImageContentFromHrefURL - Href value with HTTP or HTTPS prefix and with jpg, gif or png format
 	ImageContentFromHrefURL = "href=[\"'](http[s]?://[a-zA-Z0-9/._-]+[.](?:jpg|gif|png))[\"']"
