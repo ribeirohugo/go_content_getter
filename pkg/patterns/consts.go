40c28bd62e9472ee1189ab8ebbe3d636b156e2ba
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-09-06 22:25:39 +0100

    feat: add file from URL patterns

--- pkg/patterns/consts.go
+++ pkg/patterns/consts.go
@@ -13,4 +13,15 @@ const (
 
 	// ImageContentFromHrefURLWithoutHTTP Href value without any HTTP prefix and with jpg, gif or png format
 	ImageContentFromHrefURLWithoutHTTP = "href=[\"']([a-zA-Z0-9/._-]+[.](?:jpg|gif|png))[\"']"
+
+	// FileExtensionFromURL gets file extension from URL.
+	// It ignores domain TLD, if no file exists.
+	FileExtensionFromURL = `(?i)^[a-z][a-z0-9+\-.]*://[^/]+(?:/.*)?/[^/\\.][^/]*\.([A-Za-z0-9]+)(?:[?#].*)?$`
+
+	// FileNameFromURL gets file name, without extension.
+	FileNameFromURL = `(?i)^[a-z][a-z0-9+\-.]*://[^/]+(?:/.*)?/([^/.][^/?#]*?)(?:\.[^/?#]+)*(?:[?#].*)?$`
+
+	// FullFilenameFromURL gets complete file name, with the name and extension.
+	// Includes filenames starting with a dot (hidden files)
+	FullFilenameFromURL = `(?i)^[a-z][a-z0-9+\-.]*://[^/]+(?:/.*)?/([^/?#]+)(?:[?#].*)?$`
 )
