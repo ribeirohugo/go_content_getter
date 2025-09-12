6aff0f999ae7548be3aafde17cbd6e1749811d9e
Hugo Ribeiro <ribeirohugo@outlook.pt>
2025-09-12 17:25:25 +0100

    feat: add `DownloadVideo` and `DownloadAudio` videos methods and refactor DownloadYoutubeVideo` method name

--- pkg/video/video.go
+++ pkg/video/video.go
@@ -8,7 +8,7 @@ import (
 	"os/exec"
 )
 
-func (y Youtube) GetVideoInfo(url string) (Video, error) {
+func (y Getter) GetVideoInfo(url string) (Video, error) {
 	cmd := exec.Command("yt-dlp", "-j", url)
 
 	data, err := cmd.Output()
@@ -24,7 +24,7 @@ func (y Youtube) GetVideoInfo(url string) (Video, error) {
 	return video, nil
 }
 
-func (y Youtube) DownloadVideo(url, videoFormat, audioFormat string) ([]byte, error) {
+func (y Getter) DownloadYoutubeVideo(url, videoFormat, audioFormat string) ([]byte, error) {
 	format := fmt.Sprintf("%s+%s", videoFormat, audioFormat)
 	if videoFormat == "" {
 		format = audioFormat
@@ -41,6 +41,46 @@ func (y Youtube) DownloadVideo(url, videoFormat, audioFormat string) ([]byte, er
 		url,
 	)
 
+	return downloadBytes(cmd)
+}
+
+func (y Getter) DownloadVideo(url, videoQuality, audioQuality string) ([]byte, error) {
+	format := fmt.Sprintf(
+		"bestvideo[ext=mp4][height<=%s]+bestaudio[abr<=%s]/best[ext=mp4][height<=%s]",
+		videoQuality,
+		audioQuality,
+		videoQuality,
+	)
+
+	log.Printf("yt-dlp -f %s -o - %s", format, url)
+
+	cmd := exec.Command("yt-dlp",
+		"-f", format,
+		"-o", "-",
+		url,
+	)
+
+	return downloadBytes(cmd)
+}
+
+func (y Getter) DownloadAudio(url, audioFormat string) ([]byte, error) {
+	format := fmt.Sprintf(
+		"-f bestaudio[abr<=%s]/best[abr<=%s]",
+		audioFormat,
+		audioFormat,
+	)
+
+	cmd := exec.Command("yt-dlp",
+		"-f", format,
+		"-x", "--audio-format", "mp3",
+		"-o", "-",
+		url,
+	)
+
+	return downloadBytes(cmd)
+}
+
+func downloadBytes(cmd *exec.Cmd) ([]byte, error) {
 	var out bytes.Buffer    // will hold the video bytes
 	var stderr bytes.Buffer // will hold yt-dlp logs
 
